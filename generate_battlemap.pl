507c30bcf91cc4536c7e8f70e49101bf0bdd44e2
Jamie Reid <jamie@cwolf.org>
2011-06-30 17:54:57 +1000

    added generate_battlemap.pl

---
+++ generate_battlemap.pl
@@ -0,0 +1,60 @@
+#!/usr/bin/perl -w
+
+######################################################################
+#   generate_battlemap.pl - Generate an svg file of a 1"
+#                           battlemap suitable for use in
+#                           tabletop games like D&D.
+#
+######################################################################
+
+#############
+## includes
+#############
+
+use strict;
+use warnings;
+use Getopt::Long;
+
+#############
+## vars
+#############
+
+my ($opt_output_file, $opt_page_size);
+my $opt_random_skew			= 0;
+my $opt_help				= 0;
+
+#############
+## main
+#############
+
+## get options from the cli
+GetOptions  (   'output-file=s'		=> \$opt_output_file,		# file to output to (aotherwise print to STDOUT)
+                'page-size=s'		=> \$opt_page_size,		# Size of page, should be a4|a3|a2|a1|a0
+                'random-skew'		=> \$opt_random_skew,		# Put a spin on a traditional map
+                'help|?'		=> \$opt_help			# need help?
+            );
+
+## check options and preceed accordingly
+usage() if (!defined($opt_page_size) || $opt_help == 1);
+
+
+#############
+## subs
+#############
+
+sub usage
+{
+    print STDERR <<EOM;
+    
+Usage: generate_battlemap.pl [OPTION]...
+Generate an svg of a 1" battlemap suitable for use in atabletop games like D&D.
+
+Mandatory arugments to long options are mandatory to short options too.
+    -o, --output-file           File name to outout to (if not specified, print to STDOUT).
+    -p, --page-size             Size of map to generate (A4, A3, A2, A1, A0).
+    -r, --random-skew           Put a small spin on the traditional map.
+    -h, -?, --help              Show this information.
+    
+EOM
+    exit(0);
+}
